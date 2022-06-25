e14381612cb9b947af13fddd1c4b47bd72fe9045
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-24 22:14:45 -0600

    fixed tags again

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,9 +24,17 @@ res.status(200).json(tagData);
 });
 
 router.get('/:id', async (req, res) => {
+
+  
   // find a single tag by its `id`
-try {
-  const tagData = await Tag.findByPk(req.params.id, {include: [{ model: Product}],});
+// try {
+  const tagData = await Tag.findByPk(req.params.id, 
+    {include: [{ 
+      model: Product, 
+      as: "products"
+    }
+    ]
+    });
 
   if (!tagData) {
     res.status(404).json({message: "Not Found"});
@@ -35,11 +43,12 @@ try {
 
 res.status(200).json(tagData);
 
-} catch (err) {
-  res.status(500).json(err);
+// } catch (err) {
+//   res.status(500).json(err);
 
-} 
+// } 
 });
+
 
 router.post('/', async (req, res) => {
   // create a new tag
